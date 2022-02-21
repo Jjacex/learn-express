708a94b0ff1c8f0123fd595ea12ebd15163f3a8e
Jjacex <jacexecaj@gmail.com>
2022-02-20 22:49:27 -0600

    added more info to users.js

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,47 @@ router.get('/new', (req,res) => {
     res.send("user new form")
 })
 
+//the colon syntax below helps us to create dynamic routes
+//you can access the id by using the req.params.<your dynamic url> syntax
+//IMPORTANT NOTE: you have to set these at the bottom of your code
+//if you do not, this might overide and routes you have above it
+//long story short, put all dynamic routes below static routs
+// router.get('/:id', (req, res) => {
+//     res.send(`getting user with id of ${req.params.id}`)
+// })
+
+//basic put request
+// router.put("/:id", (req, res) => {
+//     res.send(`updating user with id of ${req.params.id}`)
+// })
+
+//basic delete request
+// router.delete("/:id", (req, res) => {
+//     res.send(`deleting user with an id of ${req.params.id}`)
+// })
+
+
+//the below syntax is helpful when we have routes which all use the same variable
+//if you look above, you can see we have commented out the original routes
+//these were all using the id parameter, so instead, we included them all into one run
+//this uses the router.route method
+router.route("/:id")
+    .get((req, res) => {
+        console.log(req.user)
+        res.send(`getting user with id of ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`updating user with id of ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`deleting user with id of ${req.params.id}`)
+    })
+
+
+const users = [{name: "kyle"}, {name: "sally"}]
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
 module.exports = router
